cb52f6e352053c7d6db45747c6f5adbea9f28a66
Teertha-193 <teerthavendra@gmail.com>
2024-09-25 12:01:20 +0530

    Update vision.py

--- vision.py
+++ vision.py
@@ -7,7 +7,7 @@ import google.generativeai as genai
 from PIL import Image
 
 genai.configure(api_key=os.getenv(key="GOOGLE_API_KEY"))
-model=genai.GenerativeModel("gemini-pro-vision")
+model=genai.GenerativeModel("gemini-1.5-flash")
 
 def get_gemini_response(input,image):
     if input!="":
