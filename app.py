d18721c8fb0ae9eb3883c8345dd6b5e4006ad795
Teertha-193 <teerthavendra@gmail.com>
2024-09-25 10:37:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
 from dotenv import load_dotenv
 load_dotenv()
+
+import streamlit as slt
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv(key="GOOGLE_API_KEY"))
+model=genai.GenerativeModel("gemini-pro")
+
+def get_gemini_response(question):
+    response=model.generate_content(question)
+    return response.txt
+
+slt.set_page_config(page_title="Q&A Demo")
+slt.header("Gemini LLM Application")
+input=slt.text_input("Input :", key='input')
+submit=slt.button("Ask the question")
+
+if submit:
+    response=get_gemini_response(input)
+    slt.subheader('The response is')
+    slt.write(response)
+    
