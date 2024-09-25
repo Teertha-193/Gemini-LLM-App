5c11b26328b978ea17a03c9cc121b34568e0ac47
Teertha-193 <teerthavendra@gmail.com>
2024-09-25 11:00:30 +0530

    Create vision.py

---
+++ vision.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as slt
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv(key="GOOGLE_API_KEY"))
+model=genai.GenerativeModel("gemini-pro-vision")
+
+def get_gemini_response(input,image):
+    if input!="":
+      response=model.generate_content([input,image])
+    else:
+      response=model.generate_content(image)
+    return response.text
+
+slt.set_page_config(page_title="Gemini Image Demo")
+slt.header("Gemini LLM Application")
+input=slt.text_input("Input :", key='input')
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+if uploaded_image is not None:
+    st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+submit=slt.button("Tell me about the image")
+
+if submit:
+  response=get_gemini_response(input, image)
+  slt.subheader("The respose is")
+  slt.write(response)
+  
