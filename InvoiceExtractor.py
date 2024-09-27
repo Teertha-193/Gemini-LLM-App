b6df7a94b357e41af483d1973871ebc7007f8591
Teertha-193 <teerthavendra@gmail.com>
2024-09-27 14:47:04 +0530

    Update InvoiceExtractor.py

--- InvoiceExtractor.py
+++ InvoiceExtractor.py
@@ -9,48 +9,27 @@ from PIL import Image
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel('google-1.5-flash')
 
-def get_gemini_response(input_prompt, image):
-    try:
-        response = model.generate_content(input_prompt, image)
-        return response.text
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-        return None
-
-def handle_uploaded_image(uploaded_image):
-    if uploaded_image is not None:
-        allowed_types = ["jpg", "jpeg", "png"]
-        if uploaded_image.type not in allowed_types:
-            st.error(f"Invalid file type. Please upload a jpg, jpeg, or png image.")
-            return None
-
-        try:
-            return Image.open(uploaded_image)
-        except Exception as e:
-            st.error(f"Error opening image: {e}")
-            return None
+def get_gemini_response(input,image):
+    if input!="":
+      response=model.generate_content([input,image])
     else:
-        return None
-
+      response=model.generate_content(image)
+    return response.text
 st.set_page_config(page_title='Multilingual Invoice Extractor')
 st.header('Multilingual Invoice Extractor')
-
-input_prompt = """
+input="""
 You are an expert in understanding the invoices. We will upload a image of invoice 
 and you will have to answer any questions based on the uploaded invoice image
 """
 
-# input_text = st.text_input("Input Prompt:", key='input')
-uploaded_image = st.file_uploader("Choose the image of invoice ", type=["jpg", "jpeg", "png"])
-
+uploaded_image = slt.file_uploader("Choose the image of invoice", type=["jpg", "jpeg", "png"])
 if uploaded_image is not None:
-    image = handle_uploaded_image(uploaded_image)
-    if image is not None:
-        st.image(uploaded_image, caption='Uploaded Invoice', use_column_width=True)
-submit = st.button("Tell me about the invoice")
+    image=Image.open(uploaded_image)
+    slt.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+submit=slt.button("Tell me about the image")
 
 if submit:
-    response = get_gemini_response(input_prompt, image)
-    if response:
-        st.subheader('The response is')
-        st.write(response)
+  response=get_gemini_response(input,image)
+  slt.subheader("The respose is")
+  slt.write(response)
+  
