9ceca6a25be8145201d050c3d32b5305414011c0
Teertha-193 <teerthavendra@gmail.com>
2024-09-27 15:46:08 +0530

    Update chat_with_pdf.py

--- chat_with_pdf.py
+++ chat_with_pdf.py
@@ -5,6 +5,7 @@ import os
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
+pip install -U langchain-community
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGeneratieAI
 from langchain.chains.question_answering import load_qa_chain
